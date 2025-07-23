26471c566a37bfa4dea1d829fbb54e700c71c8b7
vidankov <slavavinsent@gmail.com>
2025-07-23 14:40:17 +0300

    Реализована запись Location для описания локации

---
+++ Domain/ValueObjects/Location.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.ValueObjects
+{
+    public record Location
+    {
+        public string City { get; } = default;
+        public string Street { get; } = default;
+        
+        public Location(string city, string street)
+        {
+            City = city;
+            Street = street;
+        }
+
+        public static Location Of(string city, string street)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(city);
+            ArgumentException.ThrowIfNullOrWhiteSpace(street);
+
+            return new Location(city, street);
+        }
+    }
+}
