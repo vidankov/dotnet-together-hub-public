1d12120e0e8dd9ec0558f5b7de8ab81c56721404
vidankov <slavavinsent@gmail.com>
2025-07-23 14:33:51 +0300

    Реализован класс DomainException для пользовательских исключений

---
+++ Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Exceptions
+{
+    internal class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base($"Domain exception: ({message}).")
+        {
+        }
+    }
+}
