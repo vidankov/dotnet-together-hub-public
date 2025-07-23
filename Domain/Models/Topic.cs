1fe35eefb0758adb39771201f16c4ed4ac1f4bdf
vidankov <slavavinsent@gmail.com>
2025-07-23 15:11:09 +0300

    Добавлен каталог Models для моделей, Реализован класс Topic для предоставления топиков

---
+++ Domain/Models/Topic.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+
+namespace Domain.Models
+{
+    public class Topic : Entity<TopicId>
+    {
+        public string Title { get; set; } = default!;
+        public DateTime? EventStart { get; set; } = default!;
+        public string Summary { get; set; } = default!;
+        public string TopicType { get; set; } = default!;
+        public Location Location { get; set; } = default!;
+
+        public static Topic Create(
+            TopicId id, string title, DateTime eventStart,
+            string summary, string topicType, Location location)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(title);
+            ArgumentException.ThrowIfNullOrWhiteSpace(summary);
+            ArgumentException.ThrowIfNullOrWhiteSpace(topicType);
+
+            Topic topic = new()
+            {
+                Id = id,
+                Title = title,
+                EventStart = eventStart,
+                TopicType = topicType,
+                Location = location,
+                Summary = summary
+            };
+
+            return topic;
+        }
+    }
+}
