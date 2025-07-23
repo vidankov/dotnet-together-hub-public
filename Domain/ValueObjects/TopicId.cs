53d001d2e7d70d470a4e14304edd24c5d3c6fcaf
vidankov <slavavinsent@gmail.com>
2025-07-23 14:26:29 +0300

    Добавлен каталог ValueObjects, реализована запись TopicId

---
+++ Domain/ValueObjects/TopicId.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.ValueObjects
+{
+    public record TopicId
+    {
+        public Guid Value { get; }
+
+        private TopicId(Guid value)
+        {
+            Value = value;
+        }
+
+        public static TopicId Of(Guid value)
+        {
+            return new TopicId(value);
+        }
+    }
+}
