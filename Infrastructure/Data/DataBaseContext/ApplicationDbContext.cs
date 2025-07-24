3295bd41a81f0c0524d9e198b2763a993188d382
vidankov <slavavinsent@gmail.com>
2025-07-24 12:13:47 +0300

    Реализованы каталоги Data -> DataBaseContext в слое Infrastructure, Реализован класс ApplicationDbContext

---
+++ Infrastructure/Data/DataBaseContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.DataBaseContext
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Topic> Topics => Set<Topic>();
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
