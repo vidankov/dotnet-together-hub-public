2cec1440d01e93416f3becb11935e8f3a87d03d7
vidankov <slavavinsent@gmail.com>
2025-07-24 11:58:47 +0300

    Добавлена иерархия каталогов Data -> DataBaseContext в пространство Application, Реализован интерфейс IApplicationDbContext

---
+++ Application/Data/DataBaseContext/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data.DataBaseContext
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Topic> Topics { get; }
+    }
+}
