47d2f747697b7c9615c5c266bbc377b0cf912f4a
vidankov <slavavinsent@gmail.com>
2025-07-24 12:21:27 +0300

    Контекст базы данных подключен к контейнеру в Program.cs, Строка подключения добавлена в appsettings.json

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,14 +1,19 @@
+using Infrastructure.Data.DataBaseContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString(
+        "SqLiteConnection"
+    ));
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
