08c762c7b243b6b3f1ac9791330515909443ff80
vidankov <slavavinsent@gmail.com>
2025-07-30 12:00:33 +0300

    Добавлена настройка для регистрации сервиса TopicService

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Application.Topics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ITopicsService, TopicsService>();
+            return services;
+        }
+    }
+}
