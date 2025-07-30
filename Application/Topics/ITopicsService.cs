2c88063cbc6b1bd9194dcfcfac5c766d286eee6a
vidankov <slavavinsent@gmail.com>
2025-07-30 11:41:06 +0300

    Начата реализация сервиса для работы с топиками

---
+++ Application/Topics/ITopicsService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.ValueObjects;
+
+namespace Application.Topics
+{
+    public interface ITopicsService
+    {
+        Task<List<Topic>> GetTopics();
+        Task<Topic> GetTopicAsync(TopicId id);
+        Task<Topic> CreateTopicAsync(Topic topicRequestDto);
+        Task<Topic> UpdateTopicAsync(TopicId id, Topic topicRequestDto);
+        Task DeleteTopicAsync(TopicId id);
+    }
+}
---
+++ Application/Topics/TopicsService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.ValueObjects;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Topics
+{
+    public class TopicsService(IApplicationDbContext dbContext,
+        ILogger<TopicsService> logger) : ITopicsService
+    {
+        public Task<Topic> CreateTopicAsync(Topic topicRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTopicAsync(TopicId id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Topic> GetTopicAsync(TopicId id)
+        {
+            
+        }
+
+        public async Task<List<Topic>> GetTopics()
+        {
+            var topics = await dbContext.Topics
+                .AsNoTracking()
+                .ToListAsync();
+
+            return topics;
+        }
+
+        public Task<Topic> UpdateTopicAsync(TopicId id, Topic topicRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
