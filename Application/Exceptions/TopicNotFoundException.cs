aec242117f7e6633aaffc0e71c40e67b06c32999
vidankov <slavavinsent@gmail.com>
2025-07-31 11:51:36 +0300

    Добавлено исключение для ошибок, связанных с тем, что топик не найден

---
+++ Application/Exceptions/TopicNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Exceptions
+{
+    public class TopicNotFoundException : NotFoundException
+    {
+        public TopicNotFoundException(string message) : base(message)
+        {
+        }
+
+        public TopicNotFoundException(Guid id)
+            : base($"Топик с id ({id}) не найден")
+        {
+        }
+    }
+}
