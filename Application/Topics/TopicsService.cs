4732e18eda8bbcf427538dfae9c40b56c6422636
vidankov <slavavinsent@gmail.com>
2025-08-04 14:00:16 +0300

    Реализован метод и endpoint для обновления (update) топика

--- Application/Topics/TopicsService.cs
+++ Application/Topics/TopicsService.cs
@@ -53,9 +53,29 @@ namespace Application.Topics
             return topics.ToTopicResponseDtoList();
         }
 
-        public Task<TopicResponseDto> UpdateTopicAsync(Guid id, Topic topicRequestDto)
+        public async Task<TopicResponseDto> UpdateTopicAsync(Guid id, UpdateTopicDto dto)
         {
-            throw new NotImplementedException();
+            TopicId  topicId = TopicId.Of(id);
+
+            var topic = await dbContext.Topics.FindAsync(topicId);
+
+            if (topic is null)
+            {
+                throw new TopicNotFoundException(id);
+            }
+
+            topic.Title = dto.Title ?? topic.Title;
+            topic.Summary = dto.Summary ?? topic.Summary;
+            topic.TopicType = dto.TopicType ?? topic.TopicType;
+            topic.EventStart = dto.EventStart;
+            topic.Location = Location.Of(
+                dto.Location.City,
+                dto.Location.Street
+            );
+
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+
+            return topic.ToTopicResponseDto();
         }
     }
 }
