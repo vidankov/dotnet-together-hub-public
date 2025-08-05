421611b54794f5480d80774735a3f32225cdbbdd
vidankov <slavavinsent@gmail.com>
2025-08-05 15:26:15 +0300

    Добавлены свойства IsDeleted и DeletedAt к сущности Entity

--- Domain/Abstractions/Entity.cs
+++ Domain/Abstractions/Entity.cs
@@ -3,5 +3,9 @@
     public abstract class Entity<T> : IEntity<T>
     {
         public required T Id { get; set; }
+
+        public bool IsDeleted { get; set; } = default!;
+
+        public DateTimeOffset? DeletedAt { get; set; } = default!;
     }
 }
