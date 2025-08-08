c4c9c048ed712878a9fe45c47e371ccab508816f
vidankov <slavavinsent@gmail.com>
2025-08-08 12:14:38 +0300

    Реализован интерфейс ICommand

---
+++ Shared/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Shared.CQRS
+{
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+
+    public interface ICommand : IRequest<Unit>
+    {
+    }
+}
