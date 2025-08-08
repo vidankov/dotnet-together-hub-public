7b4600a1c4be767713e7402a275bab9f0a6e8624
vidankov <slavavinsent@gmail.com>
2025-08-08 12:23:16 +0300

    Реализован интерфейс IQueryHandler

---
+++ Shared/CQRS/IQuerryHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.CQRS
+{
+    public interface IQueryHandler<in TQuerry, TResponse>
+        : IRequestHandler<TQuerry, TResponse>
+        where TQuerry : IQuerry<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
