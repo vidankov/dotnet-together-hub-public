1ad0107ae67dc0c7de676bd9eeedcb2d2805b827
vidankov <slavavinsent@gmail.com>
2025-08-12 13:20:14 +0300

    Реализована команда для создания топика и её обработчик

---
+++ Application/Topics/Commands/CreateTopic/CreateTopicCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Topics.Commands.CreateTopic
+{
+    public record CreateTopicCommand(CreateTopicDto TopicDto)
+        : ICommand<CreateTopicResult>;
+
+    public record CreateTopicResult(TopicResponseDto Result);
+}
---
+++ Application/Topics/Commands/CreateTopic/CreateTopicHandler.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Application.Topics.Commands.CreateTopic
+{
+    public class CreateTopicHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<CreateTopicCommand, CreateTopicResult>
+    {
+        public async Task<CreateTopicResult> Handle(
+            CreateTopicCommand request, CancellationToken cancellationToken)
+        {
+            var newTopic = CreateTopic(request.TopicDto);
+            dbContext.Topics.Add(newTopic);
+            await dbContext.SaveChangesAsync(CancellationToken.None);
+            
+            return new CreateTopicResult(newTopic.ToTopicResponseDto());
+        }
+
+        private Topic CreateTopic(CreateTopicDto dto)
+        {
+            return Topic.Create(
+                TopicId.Of(Guid.NewGuid()),
+                dto.Title,
+                dto.EventStart,
+                dto.Summary,
+                dto.TopicType,
+                Location.Of(dto.Location.City, dto.Location.Street)
+            );
+        }
+    }
+}
