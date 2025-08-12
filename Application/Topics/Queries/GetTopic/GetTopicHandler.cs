527c05ad83f4b57922704ca216ecc34b1528ca90
vidankov <slavavinsent@gmail.com>
2025-08-12 12:10:11 +0300

    Реализованы запрос и обработчик для получения топика по id

---
+++ Application/Topics/Queries/GetTopic/GetTopicHandler.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Application.Topics.Queries.GetTopic
+{
+    public class GetTopicHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetTopicQuery, GetTopicResult>
+    {
+        public async Task<GetTopicResult> Handle(
+            GetTopicQuery request, CancellationToken cancellationToken)
+        {
+            TopicId topicId = TopicId.Of(request.id);
+
+            var result = await dbContext.Topics.FindAsync([topicId], cancellationToken);
+
+            if (result is null || result.IsDeleted)
+            {
+                throw new TopicNotFoundException(request.id);
+            }
+
+            return new GetTopicResult(result.ToTopicResponseDto());
+        }
+    }
+}
---
+++ Application/Topics/Queries/GetTopic/GetTopicQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Topics.Queries.GetTopic
+{
+    public record GetTopicQuery(Guid id) : IQuery<GetTopicResult>;
+
+    public record GetTopicResult(TopicResponseDto Topic);
+}
