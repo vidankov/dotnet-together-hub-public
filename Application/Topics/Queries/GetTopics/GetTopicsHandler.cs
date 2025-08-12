d84837b499d630888547bcf46df99b6b6974eeaa
vidankov <slavavinsent@gmail.com>
2025-08-12 10:53:58 +0300

    Реализована команда и её обработчик для получения списка всех топиков

---
+++ Application/Topics/Queries/GetTopics/GetTopicsHandler.cs
@@ -0,0 +1,19 @@
+﻿using Application.Data.DataBaseContext;
+using Shared.CQRS;
+
+namespace Application.Topics.Queries.GetTopics
+{
+    public class GetTopicsHandler(IApplicationDbContext dbContext)
+        : IQueryHandler<GetTopicsQuery, GetTopicsResult>
+    {
+        public async Task<GetTopicsResult> Handle(GetTopicsQuery request, CancellationToken cancellationToken)
+        {
+            var topics = await dbContext.Topics
+                .AsNoTracking()
+                .Where(t => !t.IsDeleted)
+                .ToListAsync(cancellationToken);
+
+            return new GetTopicsResult(topics.ToTopicResponseDtoList());
+        }
+    }
+}
---
+++ Application/Topics/Queries/GetTopics/GetTopicsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Shared.CQRS;
+
+namespace Application.Topics.Queries.GetTopics
+{
+    public record GetTopicsQuery(/*CancellationToken ct*/) : IQuery<GetTopicsResult>;
+
+    public record GetTopicsResult(List<TopicResponseDto> Topics);
+}
